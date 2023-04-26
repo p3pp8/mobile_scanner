3e07601a6c5aaf6a3d19019ab74cdffaeb5492f8
Giuseppe Marletta <giuseppe.marletta@a-tono.com>
2023-04-26 10:18:08 +0200

    Restored default analysis surface resolution.

--- android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
+++ android/src/main/kotlin/dev/steenbakker/mobile_scanner/MobileScanner.kt
@@ -193,6 +193,8 @@ class MobileScanner(
 			val camProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_1080P)
             analysisBuilder.setMaxResolution(Size(camProfile.videoFrameWidth,
                 camProfile.videoFrameHeight))
+            analysisBuilder.setDefaultResolution(Size(camProfile.videoFrameWidth,
+                camProfile.videoFrameHeight))
 
 			val analysis = analysisBuilder.build().apply { setAnalyzer(executor, captureOutput) }
 
